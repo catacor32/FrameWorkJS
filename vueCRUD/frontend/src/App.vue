c7800f3e203792b263a4fb8dd2f19b5acc1c2f0f
catacor32 <cata_cor32@yahoo.com>
2023-09-08 09:50:02 +0300

    Curs5

---
+++ vueCRUD/frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Navbar />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  <Footer />
+</template>
+
+<script>
+// import Home from './components/Home.vue'
+import Navbar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // Home,
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
